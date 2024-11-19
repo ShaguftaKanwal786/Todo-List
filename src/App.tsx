a4e4241b76d8d0d86e322c0e7e5a8437486fa908
ShaguftaKanwal786 <asimmmkhan6@gmail.com>
2024-11-19 10:03:17 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import TodoApp from "./components/Todo"
+import "./App.css";
+
+
+export default function App(){
+  return(
+    <div>
+      <div className="App"></div>
+      <h1>My To-Do List Project</h1>
+    < TodoApp />
+
+    
+    </div>
+   
+  )
+}
